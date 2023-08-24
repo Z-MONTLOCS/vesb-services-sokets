bce1f6f053a96f354180fdada75e687107ba3289
z-montlocs <agw.developer@gmail.com>
2023-08-24 15:09:52 -0500

    v-1.0

--- websockets_vesb/settings.py
+++ websockets_vesb/settings.py
@@ -59,7 +59,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -71,7 +70,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 
 
 ]
