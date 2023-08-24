9d64c9fd89f6661670e0aa3cd616f7353553db49
z-montlocs <agw.developer@gmail.com>
2023-08-24 18:57:11 -0500

    v-1.0

--- websockets_vesb/settings.py
+++ websockets_vesb/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
-PORT = os.environ.get('PORT', default=8765)
 
 
 CHROME_PATH = os.environ.get('CHROME_PATH',  default='/opt/render/project/bin/chrome-linux64')
